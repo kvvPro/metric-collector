07d17073f6a8a55973613b354482aaaac6503e84
apostol <koryabkin.vlad@gmail.com>
2023-07-15 01:34:53 +0300

    set empty body {} for request /updates/

--- cmd/server/app/handlers.go
+++ cmd/server/app/handlers.go
@@ -211,6 +211,7 @@ func (srv *Server) UpdateBatchJSONHandle(w http.ResponseWriter, r *http.Request)
 	Sugar.Infoln("body-response: ", body)
 
 	// io.WriteString(w, body)
+	w.Write([]byte("{}"))
 	w.WriteHeader(http.StatusOK)
 }
 
