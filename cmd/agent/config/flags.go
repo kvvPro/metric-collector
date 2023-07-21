efef1ca17dde13afe968a17ca42d86a776f3b68f
apostol <koryabkin.vlad@gmail.com>
2023-07-21 23:37:08 +0300

    change order read in client flags

--- cmd/agent/config/flags.go
+++ cmd/agent/config/flags.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/caarlos0/env/v8"
 	"github.com/spf13/pflag"
@@ -17,31 +16,15 @@ type ClientFlags struct {
 
 func Initialize() ClientFlags {
 	agentFlags := new(ClientFlags)
-	// try to get vars from env
-	if err := env.Parse(agentFlags); err != nil {
-		panic(err)
-	}
-	fmt.Println("ENV-----------")
-	fmt.Printf("ADDRESS=%v", agentFlags.Address)
-	fmt.Printf("\nREPORT_INTERVAL=%v", agentFlags.ReportInterval)
-	fmt.Printf("\nPOLL_INTERVAL=%v", agentFlags.PollInterval)
-	fmt.Printf("\nKEY=%v", agentFlags.HashKey)
+
 	// try to get vars from Flags
-	if _, isSet := os.LookupEnv("ADDRESS"); !isSet {
-		pflag.StringVarP(&agentFlags.Address, "addr", "a", "localhost:8080", "Net address host:port")
-	}
-	if _, isSet := os.LookupEnv("REPORT_INTERVAL"); !isSet {
-		pflag.IntVarP(&agentFlags.ReportInterval, "reportInterval", "r", 10,
-			"Wait interval in seconds before pushing metrics to server")
-	}
-	if _, isSet := os.LookupEnv("POLL_INTERVAL"); !isSet {
-		pflag.IntVarP(&agentFlags.PollInterval, "pollInterval", "p", 2,
-			"Wait interval in seconds before reading metrics")
-	}
-	if _, isSet := os.LookupEnv("KEY"); !isSet {
-		pflag.StringVarP(&agentFlags.HashKey, "key", "k", "",
-			"Hash key to calculate hash sum")
-	}
+	pflag.StringVarP(&agentFlags.Address, "addr", "a", "localhost:8080", "Net address host:port")
+	pflag.IntVarP(&agentFlags.ReportInterval, "reportInterval", "r", 10,
+		"Wait interval in seconds before pushing metrics to server")
+	pflag.IntVarP(&agentFlags.PollInterval, "pollInterval", "p", 2,
+		"Wait interval in seconds before reading metrics")
+	pflag.StringVarP(&agentFlags.HashKey, "key", "k", "",
+		"Hash key to calculate hash sum")
 
 	pflag.Parse()
 
@@ -51,6 +34,17 @@ func Initialize() ClientFlags {
 	fmt.Printf("\nPOLL_INTERVAL=%v", agentFlags.PollInterval)
 	fmt.Printf("\nKEY=%v", agentFlags.HashKey)
 	fmt.Println()
+
+	// try to get vars from env
+	if err := env.Parse(agentFlags); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("ENV-----------")
+	fmt.Printf("ADDRESS=%v", agentFlags.Address)
+	fmt.Printf("\nREPORT_INTERVAL=%v", agentFlags.ReportInterval)
+	fmt.Printf("\nPOLL_INTERVAL=%v", agentFlags.PollInterval)
+	fmt.Printf("\nKEY=%v", agentFlags.HashKey)
 
 	return *agentFlags
 }
--- cmd/server/config/flags.go
+++ cmd/server/config/flags.go
@@ -19,25 +19,15 @@ type ServerFlags struct {
 func Initialize() ServerFlags {
 	srvFlags := new(ServerFlags)
 	// try to get vars from Flags
-	// if _, isSet := os.LookupEnv("ADDRESS"); !isSet {
 	pflag.StringVarP(&srvFlags.Address, "addr", "a", "localhost:8080", "Net address host:port")
-	// }
-	// if _, isSet := os.LookupEnv("DATABASE_DSN"); !isSet {
 	// pflag.StringVarP(&srvFlags.DBConnection, "databaseConn", "d", "user=postgres password=postgres host=localhost port=5432 dbname=postgres sslmode=disable", "Connection string to DB: user=<> password=<> host=<> port=<> dbname=<>")
 	pflag.StringVarP(&srvFlags.DBConnection, "databaseConn", "d", "", "Connection string to DB: user=<> password=<> host=<> port=<> dbname=<>")
-	// }
-	// if _, isSet := os.LookupEnv("STORE_INTERVAL"); !isSet {
 	pflag.IntVarP(&srvFlags.StoreInterval, "storeInterval", "i", 5,
 		"Wait interval in seconds before dropping metrics to file")
-	// }
-	// if _, isSet := os.LookupEnv("FILE_STORAGE_PATH"); !isSet {
 	pflag.StringVarP(&srvFlags.FileStoragePath, "fileStoragePath", "f", "/tmp/metrics-db.json",
 		"Path to file where to save metrics")
-	// }
-	// if _, isSet := os.LookupEnv("RESTORE"); !isSet {
 	pflag.BoolVarP(&srvFlags.Restore, "restore", "r", true,
 		"True if restore values from file stored in FILE_STORAGE_PATH")
-	// }
 	pflag.StringVarP(&srvFlags.HashKey, "hashKey", "k", "",
 		"Hash key to calculate hash sum")
 
